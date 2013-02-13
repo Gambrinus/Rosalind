d3fae1ffdfdd3f1de99ee03a9b38ab34b60458c4
Daniel J Barnes <barnes.bch@gmail.com>
2013-02-13 13:40:26 -0600

    trie done

---
+++ Python/TRIE.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+""" Rosalind project - Problem: Introduction to Pattern Matching
+Problem
+Given a collection of strings, their trie (often pronounced "try" to avoid ambiguity with the general term
+tree) is a rooted tree formed as follows. For every unique first symbol in the strings, an edge is formed
+connecting the root to a new vertex. This symbol is then used to label the edge.
+We may then iterate the process by moving down one level as follows. Say that an edge connecting the root
+to a node v is labeled with 'A'; then we delete the first symbol from every string in the collection
+beginning with 'A' and then treat v as our root. We apply this process to all nodes that are adjacent to
+the root, and then we move down another level and continue. See Figure 1 for an example of a trie.
+As a result of this method of construction, the symbols along the edges of any path in the trie from the
+root to a leaf will spell out a unique string from the collection, as long as no string is a prefix of
+another in the collection (this would cause the first string to be encoded as a path terminating at an
+internal node).
+
+Given: A list of DNA strings, none of which is a prefix of another.
+Return: The adjacency list corresponding to the trie T for these patterns, in the following format. If
+T has n nodes, first label the root with 1 and then label the remaining nodes with the integers 2
+through n in any order you like. Each edge of the adjacency list of T will be encoded by a triple
+containing the integer representing the edge's parent node, followed by the integer representing the
+edge's child node, and finally the symbol labeling the edge.
+
+Sample Dataset
+ATAGA
+ATC
+GAT
+Sample Output
+1 2 A
+2 3 T
+3 4 A
+4 5 G
+5 6 A
+3 7 C
+1 8 G
+8 9 A
+9 10 T
+"""
+
+__author__ = "Daniel J. Barnes"
+__email__ = "ghen2000@gmail.com"
+__status__ = "Working"
+
+### Modules ###
+
+## Functions ##
+def rosalind_open(proj):
+	import sys
+	proj = proj.lower()
+	if len(sys.argv) >= 2 and sys.argv[1] == "-t":
+		filein = 'test_input.txt'
+	else:
+		filein = 'rosalind_' + proj + '.txt'
+	with open(filein, 'r') as f:
+		return f.read().splitlines()
+
+def rosalind_out(proj,out):
+	proj = proj.upper()
+	out = str(out)
+	fileout = proj + '_output.txt'
+	with open(fileout, 'w') as f:
+		f.write(out)
+	with open(fileout, 'r') as f:
+		print f.read()
+
+def output_nodes(root):
+	for x in root.seq:
+		out_list.append(' '.join((str(root.n),str(root.seq[x].n),str(x))))
+		output_nodes(root.seq[x])
+
+
+### Classes ###
+class Node:
+	n = 1
+
+	def __init__(self):
+		self.seq = {}
+		self.n = Node.n
+		Node.n += 1
+		
+
+	def __str__(self):
+		return 'Node ' + str(self.n) + ' ' + self.seq
+
+#### Main #####
+if __name__ == "__main__":
+	proj = 'TRIE'
+
+	seqs = rosalind_open(proj)
+
+	root = Node()
+	for seq in seqs:
+		x = root
+		for char in seq:
+			if not char in x.seq:
+				x.seq[char] = Node()
+			x = x.seq[char]
+
+	out_list = []
+	output_nodes(root)
+
+	out = '\n'.join(out_list)
+
+	rosalind_out(proj,out)
